1dd72ada2db9baf99b7c1f4a5cd024f3c2526a77
Tanuj Nayak <tanujnay112@live.com>
2026-01-22 03:42:58 -0800

    [ENH]: increment_compaction_failure_count on rust sysdb

--- rust/rust-sysdb/src/types.rs
+++ rust/rust-sysdb/src/types.rs
@@ -484,6 +484,11 @@ impl TryFrom<chroma_proto::GetCollectionWithSegmentsRequest> for GetCollectionWi
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct UpdateCollectionCursor {
+    pub compaction_failure_count_increment: Option<u32>,
+}
+
 /// Internal request for updating a collection.
 #[derive(Debug, Clone)]
 pub struct UpdateCollectionRequest {
@@ -501,6 +506,8 @@ pub struct UpdateCollectionRequest {
     pub reset_metadata: bool,
     // New configuration to set (optional - None means don't change)
     pub new_configuration: Option<UpdateCollectionConfiguration>,
+    // Cursor updates (optional - None means don't change)
+    pub cursor_updates: Option<UpdateCollectionCursor>,
 }
 
 impl TryFrom<chroma_proto::UpdateCollectionRequest> for UpdateCollectionRequest {
@@ -561,6 +568,7 @@ impl TryFrom<chroma_proto::UpdateCollectionRequest> for UpdateCollectionRequest 
             metadata,
             reset_metadata,
             new_configuration,
+            cursor_updates: None,
         })
     }
 }
